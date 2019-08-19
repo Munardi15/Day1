669f01747534b8c2ac34081fc232cf6d1980bb1b
Munardi15 <54164085+Munardi15@users.noreply.github.com>
2019-08-19 23:06:48 +0700

    Add files via upload

---
+++ Konohamaru day 1/EXERCISES 3/index.js
@@ -0,0 +1,114 @@
+/* Output untuk Input nama = '' dan peran = ''
+var nama = ''
+var peran = ''
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}*/
+
+/*Output untuk Input nama = 'Mikael' dan peran = ''
+var nama = 'Mikael'
+var peran = ''
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}*/
+
+/*Output untuk Input nama = 'Nina' dan peran 'Ksatria'
+var nama = 'Nina'
+var peran = 'Ksatria'
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}*/
+
+/*Output untuk Input nama = 'Danu' dan peran 'Tabib'
+var nama = 'Danu'
+var peran = 'Tabib'
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}*/
+
+//Output untuk Input nama = 'Zero' dan peran 'Penyihir'
+var nama = 'Zero'
+var peran = 'Penyihir'
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}
