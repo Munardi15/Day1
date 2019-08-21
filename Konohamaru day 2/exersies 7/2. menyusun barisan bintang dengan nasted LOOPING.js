f9f7732fcdc323e71a1e1b5a70066af303845379
Munardi15 <54164085+Munardi15@users.noreply.github.com>
2019-08-21 14:48:52 +0700

    Update 2. menyusun barisan bintang dengan nasted LOOPING.js

--- Konohamaru day 2/exersies 7/2. menyusun barisan bintang dengan nasted LOOPING.js
+++ Konohamaru day 2/exersies 7/2. menyusun barisan bintang dengan nasted LOOPING.js
@@ -1,13 +1,5 @@
 //2. menyusun barisan bintang dengan nasted LOOPING
-var b = 5;
-var i=0;
-while (i<b) {
-  var asteriks = '*';
-  var j = 1;
-  while (j<b) {
-    asteriks = asteriks + '*';
-    j++;
-  }
-  console.log(asteriks);
-  i++;
+var s = 5;
+for (var i=1;i<=5;i++){
+  console.log('*'. repeat(s));
 }
